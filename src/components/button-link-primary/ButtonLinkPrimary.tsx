42864cddd780feb0f41d992cd5a9512de6b690e2
Michael Purcella <mjpurcella@gmail.com>
2022-11-14 09:03:08 -0800

    [#7] Refactor buttonlink primary component

--- src/components/button-link-primary/ButtonLinkPrimary.tsx
+++ src/components/button-link-primary/ButtonLinkPrimary.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-interface ButtonLinkPrimaryTypes {
+type ButtonLinkPrimaryTypes = {
   link: string;
   children: React.ReactNode;
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
   styleLight: boolean;
-}
+};
 
 const ButtonLinkPrimary = ({
   link,
@@ -15,29 +15,16 @@ const ButtonLinkPrimary = ({
   endIcon,
   styleLight,
 }: ButtonLinkPrimaryTypes) => {
-  if (startIcon !== undefined && endIcon === undefined) {
-    return (
-      <a
-        href={link}
-        className={styleLight ? "button-link-light" : "button-link-dark"}
-      >
-        <span className="mr-8">{startIcon}</span>
-        {children}
-      </a>
-    );
-  } else if (startIcon === undefined && endIcon !== undefined) {
-    return (
-      <a
-        href={link}
-        className={styleLight ? "button-link-light" : "button-link-dark"}
-      >
-        {children}
-        <span className="ml-8">{endIcon}</span>
-      </a>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <a
+      href={link}
+      className={styleLight ? "button-link-light" : "button-link-dark"}
+    >
+      {Boolean(startIcon) && <span className="mr-12">{startIcon}</span>}
+      {children}
+      {Boolean(endIcon) && <span className="ml-12">{endIcon}</span>}
+    </a>
+  );
 };
 
 export default ButtonLinkPrimary;
