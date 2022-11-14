48c6ae4e95806d7726e2e1ec1a31a1f681f3d206
Michael Purcella <mjpurcella@gmail.com>
2022-11-14 09:29:13 -0800

    [#8] Create poster component

---
+++ src/components/poster/Poster.tsx
@@ -0,0 +1,16 @@
+interface PosterTypes {
+  image: string;
+  alt: string;
+}
+
+const Poster = ({ image, alt }: PosterTypes) => {
+  return (
+    <img
+      src={image}
+      className="block w-100 rounded-8 object-center object-cover shadow-lg shadow-black/50"
+      alt={alt}
+    />
+  );
+};
+
+export default Poster;
