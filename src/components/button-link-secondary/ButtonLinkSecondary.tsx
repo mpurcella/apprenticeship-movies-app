913be54b24bf38d07f64367bcd24a1f359e2c7c4
Michael Purcella <mjpurcella@gmail.com>
2022-11-13 22:07:51 -0800

    [#46] Refactor buttonlink secondary component

--- src/components/button-link-secondary/ButtonLinkSecondary.tsx
+++ src/components/button-link-secondary/ButtonLinkSecondary.tsx
@@ -1,26 +1,22 @@
 import React from "react";
 
-interface ButtonLinkSecondaryTypes {
+type ButtonLinkSecondaryTypes = {
   link: string;
-  startIcon?: React.ReactNode;
+  startIcon: React.ReactNode;
   children: React.ReactNode;
-}
+};
 
 const ButtonLinkSecondary = ({
   link,
   startIcon,
   children,
 }: ButtonLinkSecondaryTypes) => {
-  if (startIcon !== undefined) {
-    return (
-      <a href={link} className="button-link-secondary">
-        <span className="mr-12">{startIcon}</span>
-        {children}
-      </a>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <a href={link} className="button-link-secondary">
+      <span className="mr-12">{startIcon}</span>
+      {children}
+    </a>
+  );
 };
 
 export default ButtonLinkSecondary;
