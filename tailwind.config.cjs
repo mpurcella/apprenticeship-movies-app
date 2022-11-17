d6dfb739fc8ccc203cdef59f1a219816f4b3c91d
Michael Purcella <mjpurcella@gmail.com>
2022-11-16 16:57:01 -0800

    [#8] Update and remove styles and fix props

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
     colors: {
       current: "currentColor",
       transparent: "transparent",
-      white: "#fff",
+      white: "#FFF",
       black: "#28282B",
       grey: {
         100: "#E8E8E8",
@@ -18,42 +18,11 @@ module.exports = {
       red: "#FF003F",
     },
     fontSize: {
-      10: [".625rem", ".625rem"],
-      12: [".75rem", ".75rem"],
-      13: [".8125rem", ".8125rem"],
-      14: [".875rem", ".875rem"],
-      16: ["1rem", "1rem"],
-      18: ["1.125rem", "1.125rem"],
-      20: ["1.25rem", "1.25rem"],
-      22: ["1.375rem", "1.375rem"],
-      24: ["1.5rem", "1.5rem"],
-      26: ["1.625rem", "1.625rem"],
-      28: ["1.75rem", "1.75rem"],
-      30: ["1.875rem", "1.875rem"],
-      32: ["2rem", "2rem"],
-      36: ["2.25rem", "2.25rem"],
-      40: ["2.5rem", "2.5rem"],
-      44: ["2.75rem", "2.75rem"],
-      48: ["3rem", "3rem"],
-      52: ["3.25rem", "3.25rem"],
-      56: ["3.5rem", "3.5rem"],
+      14: [".875rem"],
     },
     spacing: {
-      0: "0rem",
-      1: ".0625rem",
-      2: ".125rem",
-      4: ".25rem",
-      6: ".375rem",
-      8: ".5rem",
-      10: ".625rem",
       12: ".75rem",
-      16: "1rem",
-      20: "1.25rem",
-      24: "1.5rem",
-      28: "1.75rem",
       32: "2rem",
-      36: "2.25rem",
-      40: "2.5rem",
       100: "100%",
     },
     borderRadius: {
@@ -62,7 +31,6 @@ module.exports = {
     },
     borderWidth: {
       DEFAULT: "1px",
-      6: "6px",
     },
   },
   plugins: [
@@ -74,7 +42,7 @@ module.exports = {
 
       addComponents({
         ".button-link-secondary": {
-          "@apply w-100 text-14 text-black bg-white inline-flex py-12 px-12 ease-out duration-300 hover:bg-grey-100":
+          "@apply w-100 text-14 text-black bg-white inline-flex p-12 ease-out duration-300 hover:bg-grey-100":
             {},
         },
         ".button-link-light": {
@@ -85,9 +53,16 @@ module.exports = {
           ...buttonLinkPrimary,
           "@apply text-white bg-black hover:text-black hover:bg-white": {},
         },
-        ".button-primary": {
+        ".button": {
           ...buttonLinkPrimary,
           "@apply button-link-dark": {},
+        },
+      });
+    },
+    ({ addBase }) => {
+      addBase({
+        body: {
+          "@apply leading-none antialiased": {},
         },
       });
     },
