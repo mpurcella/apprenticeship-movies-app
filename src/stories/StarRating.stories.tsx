00d94fc0b30153aad09a083e010d7c85c1e5626b
Michael Purcella <mjpurcella@gmail.com>
2022-11-30 09:04:39 -0800

    [#16] Create star rating component

---
+++ src/stories/StarRating.stories.tsx
@@ -0,0 +1,43 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import StarRating from "../components/star-rating/StarRating";
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+export default {
+  title: "StarRating",
+  component: StarRating,
+} as ComponentMeta<typeof StarRating>;
+
+const Template: ComponentStory<typeof StarRating> = (args) => (
+  <StarRating {...args} />
+);
+
+// TMDB ratings are out of 10. StarRating divides rating by 2 in order to map stars accurately.
+export const FiveStar = Template.bind({});
+FiveStar.args = {
+  rating: 10,
+};
+
+export const FourStar = Template.bind({});
+FourStar.args = {
+  rating: 8,
+};
+
+export const ThreeStar = Template.bind({});
+ThreeStar.args = {
+  rating: 6,
+};
+
+export const TwoStar = Template.bind({});
+TwoStar.args = {
+  rating: 4,
+};
+
+export const OneStar = Template.bind({});
+OneStar.args = {
+  rating: 2,
+};
+
+export const HalfStar = Template.bind({});
+HalfStar.args = {
+  rating: 1,
+};
