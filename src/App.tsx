828ac54bd889e921621b17ffd30154965c4e5878
Michael Purcella <mjpurcella@gmail.com>
2022-12-02 14:30:51 -0800

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import Pages from "./pages/Pages";
+
 function App() {
-  return <h1>Movies App</h1>;
+  return (
+    <BrowserRouter>
+      <Pages />
+    </BrowserRouter>
+  );
 }
 
 export default App;
