b3fcd6189308e521352850d8a9be96cd915cd3a4
Michael Purcella <mjpurcella@gmail.com>
2022-12-01 16:48:07 -0800

    [#52] Refactor moviecard styling - PR #60

--- src/components/movie-card/MovieCard.tsx
+++ src/components/movie-card/MovieCard.tsx
@@ -13,7 +13,7 @@ const MovieCard = ({ imageUrl, url, title, alt, rating }: MovieCardTypes) => {
   return (
     <a
       href={url}
-      className="w-quarter rounded-8 group ease-out duration-200 hover:scale-[1.02] hover:bg-black hover:shadow-lg hover:shadow-black/50"
+      className="w-quarter rounded-8 group ease-out duration-200 hover:scale-102 hover:bg-black hover:shadow-lg hover:shadow-black/50"
     >
       <Poster imageUrl={imageUrl} alt={alt} />
       <div className="flex flex-col items-center px-12 py-20 space-y-12">
