c57afaf5fd6826fc5ff35df864bf30ee07580d8e
Michael Purcella <mjpurcella@gmail.com>
2022-12-07 10:18:53 -0800

    [#52] Refactor movie card component styling

--- src/components/movie-card/MovieCard.tsx
+++ src/components/movie-card/MovieCard.tsx
@@ -13,11 +13,11 @@ const MovieCard = ({ imageUrl, url, title, alt, rating }: MovieCardTypes) => {
   return (
     <a
       href={url}
-      className="w-quarter rounded-8 group ease-out duration-200 hover:scale-102 hover:bg-black hover:shadow-lg hover:shadow-black/50"
+      className="h-full rounded-8 group ease-out duration-200 flex flex-col relative outline-none hover:bg-black hover:scale-102 hover:shadow-lg hover:shadow-black/50 focus:bg-black focus:scale-102 lg:bg-black"
     >
       <Poster imageUrl={imageUrl} alt={alt} />
-      <div className="flex flex-col items-center px-12 py-20 space-y-12">
-        <p className="text-14 leading-snug text-center ease-out duration-200 group-hover:text-white-200">
+      <div className="flex flex-col items-center px-12 py-20 space-y-12 lg:w-full lg:h-full lg:absolute lg:justify-center lg:top-0 lg:left-0">
+        <p className="text-18 font-light leading-snug text-center ease-out duration-200 group-hover:text-white-200 group-focus:text-white-200 lg:text-20 lg:hidden lg:group-hover:block lg:group-focus:block">
           {title}
         </p>
         <StarRating rating={rating} />
