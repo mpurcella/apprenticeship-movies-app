46e9e2ae93db6f03b16fe097fc7af87e4f9604ba
Michael Purcella <mjpurcella@gmail.com>
2022-12-07 10:27:49 -0800

    [#23] Create page for upcoming movies

---
+++ src/pages/discover/Upcoming.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { axiosClient } from "../../utils/axios";
+import MovieList from "../../components/movie-list/MovieList";
+import { Movie } from "../../types/movies";
+import { useStore } from "../../store/store";
+import { CanceledError } from "axios";
+
+const Popular = () => {
+  const { config } = useStore();
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoadingMovies, setIsLoadingMovies] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const fetchMovies = async () => {
+      setIsLoadingMovies(true);
+
+      try {
+        const resp = await axiosClient.get<{ results: Movie[] }>(
+          "/movie/upcoming",
+          { signal }
+        );
+        setMovies(resp.data.results);
+      } catch (err) {
+        if (!(err instanceof CanceledError)) {
+          setError(err as Error);
+        }
+      } finally {
+        if (!signal.aborted) {
+          setIsLoadingMovies(false);
+        }
+      }
+    };
+
+    fetchMovies().catch(console.error);
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  if (isLoadingMovies) {
+    return <div>Loading...</div>;
+  }
+
+  if (error !== null) {
+    return <div>It broke...{error.message}</div>;
+  }
+
+  return (
+    <div className="flex flex-col items-center px-48 pt-102 pb-68 lg:py-56 lg:ml-260">
+      <h1 className="inline-block text-20 text-white-200 bg-black uppercase font-semibold text-center mb-36 px-8 py-4">
+        Upcoming Movies
+      </h1>
+      <MovieList movies={movies} baseUrl={config.baseUrl} />
+    </div>
+  );
+};
+
+export default Popular;
