3524cfd0c3be4b2eb4ab1bb35727190ec302e510
Michael Purcella <mjpurcella@gmail.com>
2022-12-07 10:41:42 -0800

    [#51] Create movie list component

---
+++ src/components/movie-list/MovieList.tsx
@@ -0,0 +1,33 @@
+import { Movie } from "../../types/movies";
+import MovieCard from "../movie-card/MovieCard";
+
+type MovieListTypes = {
+  movies: Movie[];
+  baseUrl: string;
+};
+
+const MovieList = ({ movies, baseUrl }: MovieListTypes) => {
+  console.log(movies);
+
+  return (
+    <>
+      <ul className="grid grid-cols-1 gap-36 sm:grid-cols-2 md:grid-cols-3 sm:gap-28 lg:grid-cols-2 xl:grid-cols-3 ">
+        {movies.map((movie) => {
+          return (
+            <li key={movie.id}>
+              <MovieCard
+                imageUrl={`${baseUrl}w780/${movie.poster_path ?? ""}`}
+                url={`/movie/${movie.id}`}
+                title={movie.title}
+                alt={movie.title}
+                rating={movie.vote_average}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default MovieList;
