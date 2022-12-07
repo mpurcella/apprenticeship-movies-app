ee9fda1834bb042f0bd00d5ddb9f62d3a877a4c1
Michael Purcella <mjpurcella@gmail.com>
2022-12-07 10:56:23 -0800

    [#45] Refactor button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 type ButtonTypes = {
+  startIcon: React.ReactNode;
   endIcon: React.ReactNode;
   children: React.ReactNode;
+  styleLight: boolean;
 };
 
-const Button = ({ endIcon, children }: ButtonTypes) => {
+const Button = ({ startIcon, endIcon, children, styleLight }: ButtonTypes) => {
   return (
-    <button className="button">
+    <button className={styleLight ? "button-light" : "button-dark"}>
+      {Boolean(startIcon) && <span className="text-14 mr-12">{startIcon}</span>}
       {children}
-      <span className="ml-12">{endIcon}</span>
+      {Boolean(endIcon) && <span className="text-14 ml-12">{endIcon}</span>}
     </button>
   );
 };
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -10,8 +10,30 @@ export default {
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const PrimaryLightStart = Template.bind({});
+PrimaryLightStart.args = {
+  children: "Button",
+  startIcon: <FaPlay />,
+  styleLight: true,
+};
+
+export const PrimaryLightEnd = Template.bind({});
+PrimaryLightEnd.args = {
   children: "Button",
   endIcon: <FaPlay />,
+  styleLight: true,
+};
+
+export const PrimaryDarkStart = Template.bind({});
+PrimaryDarkStart.args = {
+  children: "Button",
+  startIcon: <FaPlay />,
+  styleLight: false,
+};
+
+export const PrimaryDarkEnd = Template.bind({});
+PrimaryDarkEnd.args = {
+  children: "Button",
+  endIcon: <FaPlay />,
+  styleLight: false,
 };
