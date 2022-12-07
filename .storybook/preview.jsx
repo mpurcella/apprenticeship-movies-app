eb843fd49ba140c8abb2c96d4633d639171d27aa
Michael Purcella <mjpurcella@gmail.com>
2022-12-07 10:59:18 -0800

    Wrap stories in memory router

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,4 +1,5 @@
 import "../src/index.css";
+import { MemoryRouter } from "react-router-dom";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,3 +10,11 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <MemoryRouter>
+      <Story />
+    </MemoryRouter>
+  ),
+];
