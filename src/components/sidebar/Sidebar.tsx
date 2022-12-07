d5c9ab56a79b4ae9909cf3f163e2a7622c3a9615
Michael Purcella <mjpurcella@gmail.com>
2022-12-07 10:32:25 -0800

    [#15] Refactor sidebar component

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,90 +1,47 @@
-import { useState, useEffect } from "react";
-import { axiosClient } from "../../utils/axios";
 import classNames from "classnames";
-import SidebarList from "../sidebar-list/SidebarList";
 import {
-  FaTimes,
-  FaHeart,
-  FaChartBar,
   FaCalendarAlt,
+  FaChartBar,
+  FaHeart,
   FaRegCircle,
+  FaTimes,
 } from "react-icons/fa";
+import { useStore } from "../../store/store";
+import SidebarList from "../sidebar-list/SidebarList";
 
 type SideBarTypes = {
   isOpen: boolean;
   closeSidebar: () => void;
 };
 
-type GenresTypes = {
-  id: number;
-  name: string;
-};
-
 const discoverItems = [
   {
     label: "Popular",
-    url: "/movies/popular",
+    url: "/discover/popular",
     icon: <FaHeart />,
   },
   {
     label: "Top Rated",
-    url: "/movies/top-rated",
+    url: "/discover/top-rated",
     icon: <FaChartBar />,
   },
   {
     label: "Upcoming",
-    url: "/movies/upcoming",
+    url: "/discover/upcoming",
     icon: <FaCalendarAlt />,
   },
 ];
 
 const Sidebar = ({ isOpen, closeSidebar }: SideBarTypes) => {
-  const [genres, setGenres] = useState<GenresTypes[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-
-    const fetchGenres = async () => {
-      setIsLoading(true);
-
-      try {
-        const resp = await axiosClient.get<{ genres: GenresTypes[] }>(
-          "/genre/movie/list",
-          { signal }
-        );
-        setGenres(resp.data.genres);
-      } catch (err) {
-        setError(err as Error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchGenres().catch(console.error);
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error !== null) {
-    return <div>It broke... {error.message}</div>;
-  }
+  const { genres } = useStore();
 
   return (
     <div
       className={classNames(
-        "w-280 bg-black pt-48 pb-36 fixed top-0 bottom-0 transition-all linear duration-500 scrollbar-none overflow-y-scroll",
+        "w-260 bg-black pt-48 pb-36 fixed top-0 bottom-0 transition-all linear duration-500 scrollbar-none overflow-y-scroll",
         {
           "left-0 shadow-lg shadow-black/95": isOpen,
-          "-left-280": !isOpen,
+          "-left-260": !isOpen,
         }
       )}
     >
@@ -92,6 +49,7 @@ const Sidebar = ({ isOpen, closeSidebar }: SideBarTypes) => {
         type="button"
         className="text-20 text-grey-300 absolute top-8 right-8 ease-out duration-200 hover:text-white-100 lg:hidden"
         onClick={closeSidebar}
+        aria-label="Close"
       >
         <FaTimes />
       </button>
