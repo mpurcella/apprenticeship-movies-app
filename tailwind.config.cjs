e84d8124c9077f36763d610b5a1a56ca9197d72f
Michael Purcella <mjpurcella@gmail.com>
2022-12-07 11:01:08 -0800

    Add/remove styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,9 @@ module.exports = {
         "open-sans": ["Open Sans", "sans-serif"],
         dosis: ["Dosis", "sans-serif"],
       },
+      opacity: {
+        15: ".15",
+      },
     },
     colors: {
       current: "currentColor",
@@ -35,6 +38,7 @@ module.exports = {
       0: "0rem",
       2: ".125rem",
       3: ".1875rem",
+      4: ".25rem",
       8: ".5rem",
       12: ".75rem",
       20: "1.25rem",
@@ -43,50 +47,57 @@ module.exports = {
       36: "2.25rem",
       48: "3rem",
       56: "3.5rem",
-      280: "17.5rem",
+      68: "4.25rem",
+      102: "6.375rem",
+      260: "16.25rem",
       quarter: "25%",
       full: "100%",
     },
     borderRadius: {
       DEFAULT: "24px",
+      0: "0px",
       8: "8px",
     },
     scale: {
       102: "1.02",
     },
     screens: {
-      lg: "1024px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
     },
   },
   plugins: [
-    ({ addComponents }) => {
-      const buttonLinkPrimary = {
+    ({ addComponents, addUtilities, addBase }) => {
+      const buttonRoundBase = {
         "@apply inline-flex items-center py-12 px-32 border border-black rounded ease-out duration-300":
           {},
       };
 
       addComponents({
-        ".button-link-secondary": {
-          "@apply w-full text-14 text-grey-200 font-normal bg-black inline-flex py-12 px-28 ease-out hover:text-white-100 hover:bg-grey-600 focus:outline-none focus:bg-grey-600":
-            {},
-        },
         ".button-link-light": {
-          ...buttonLinkPrimary,
+          ...buttonRoundBase,
           "@apply text-black bg-white-200 hover:text-white-200 hover:bg-black":
             {},
         },
         ".button-link-dark": {
-          ...buttonLinkPrimary,
+          ...buttonRoundBase,
           "@apply text-white-200 bg-black hover:text-black hover:bg-white-200":
             {},
         },
-        ".button": {
-          ...buttonLinkPrimary,
+        ".button-link-secondary": {
+          "@apply w-full text-14 text-grey-200 font-normal bg-black inline-flex py-12 px-28 ease-out hover:text-white-100 hover:bg-grey-600 focus:outline-none focus:bg-grey-600":
+            {},
+        },
+        ".button-light": {
+          "@apply button-link-light": {},
+        },
+        ".button-dark": {
           "@apply button-link-dark": {},
         },
       });
-    },
-    ({ addUtilities }) => {
+
       addUtilities({
         ".scrollbar-none": {
           scrollbarWidth: "none",
@@ -95,8 +106,7 @@ module.exports = {
           },
         },
       });
-    },
-    ({ addBase }) => {
+
       addBase({
         body: {
           "@apply font-open-sans bg-white-100 leading-none antialiased": {},
