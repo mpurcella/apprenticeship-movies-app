ecf7a95b6a7a417ba8084e845122176a0bb6c35b
Michael Purcella <mjpurcella@gmail.com>
2022-12-07 10:29:53 -0800

    Update page routes and remove 'discover' page

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,7 +1,10 @@
 import { lazy, Suspense } from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
+import Header from "../components/header/Header";
 
-const Discover = lazy(async () => await import("./discover/Discover"));
+const Popular = lazy(async () => await import("./discover/Popular"));
+const TopRated = lazy(async () => await import("./discover/TopRated"));
+const Upcoming = lazy(async () => await import("./discover/Upcoming"));
 const Genres = lazy(async () => await import("./genres/Genres"));
 const Movie = lazy(async () => await import("./movie/Movie"));
 const Actor = lazy(async () => await import("./actor/Actor"));
@@ -9,16 +12,60 @@ const Actor = lazy(async () => await import("./actor/Actor"));
 function Pages() {
   return (
     <>
-      <Suspense fallback={null}>
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/discover" />} />
-          <Route path="/discover" element={<Discover />} />
-          <Route path="/genres" element={<Genres />} />
-          <Route path="/movie" element={<Movie />} />
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route
+            path="/"
+            element={<Navigate replace to="/discover/popular" />}
+          />
+          <Route
+            path="/discover/popular"
+            element={
+              <Suspense fallback={<div>Loading popular movies...</div>}>
+                <Popular />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/discover/top-rated"
+            element={
+              <Suspense fallback={<div>Loading top rated movies...</div>}>
+                <TopRated />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/discover/upcoming"
+            element={
+              <Suspense fallback={<div>Loading top rated movies...</div>}>
+                <Upcoming />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/genres/:genre"
+            element={
+              <Suspense fallback={<div>Loading genres..</div>}>
+                <Genres />
+              </Suspense>
+            }
+          />
+          <Route path="/movie/:id" element={<Movie />} />
           <Route path="/actor" element={<Actor />} />
-        </Routes>
-      </Suspense>
+          <Route path="/not-found" element={<div>Page not found.</div>} />
+          <Route path="*" element={<Navigate replace to="/not-found" />} />
+        </Route>
+      </Routes>
     </>
+  );
+}
+
+function MainLayout() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
   );
 }
 
