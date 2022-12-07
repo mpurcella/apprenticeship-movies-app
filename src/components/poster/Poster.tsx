8ea967132ee9e732f3eea6cbf5b63b276f8cfc0b
Michael Purcella <mjpurcella@gmail.com>
2022-12-07 10:33:17 -0800

    [#8] Refactor poster component styling

--- src/components/poster/Poster.tsx
+++ src/components/poster/Poster.tsx
@@ -7,7 +7,7 @@ const Poster = ({ imageUrl, alt }: PosterTypes) => {
   return (
     <img
       src={imageUrl}
-      className="block w-full rounded-8 object-center object-cover shadow-lg shadow-black/50"
+      className="block h-full w-full rounded-8 object-center object-cover shadow-lg shadow-black/50 ease-out duration-200 lg:group-hover:opacity-10 lg:group-focus:opacity-10"
       alt={alt}
     />
   );
