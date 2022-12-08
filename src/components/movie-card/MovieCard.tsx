83c818ce5f177624a1fb06403e570bdfc9cab485
Michael Purcella <mjpurcella@gmail.com>
2022-12-08 12:15:35 -0800

    [#52] Refactor moviecard/poster components to set standard height

--- src/components/movie-card/MovieCard.tsx
+++ src/components/movie-card/MovieCard.tsx
@@ -13,11 +13,11 @@ const MovieCard = ({ imageUrl, url, title, alt, rating }: MovieCardTypes) => {
   return (
     <a
       href={url}
-      className="h-full rounded-8 group ease-out duration-200 flex flex-col relative outline-none hover:bg-black hover:scale-102 hover:shadow-lg hover:shadow-black/50 focus:bg-black focus:scale-102 lg:bg-black"
+      className="rounded-8 group ease-out duration-200 flex flex-col relative outline-none hover:bg-black hover:scale-102 hover:shadow-lg hover:shadow-black/50 focus:bg-black focus:scale-102 lg:bg-black"
     >
       <Poster imageUrl={imageUrl} alt={alt} />
-      <div className="flex flex-col items-center px-12 py-20 space-y-12 lg:w-full lg:h-full lg:absolute lg:justify-center lg:top-0 lg:left-0">
-        <p className="text-18 font-light leading-snug text-center ease-out duration-200 group-hover:text-white-200 group-focus:text-white-200 lg:text-20 lg:hidden lg:group-hover:block lg:group-focus:block">
+      <div className="flex flex-col items-center px-12 py-20 space-y-12 lg:space-y-20 lg:w-full lg:h-full lg:absolute lg:justify-center lg:top-0 lg:left-0">
+        <p className="text-16 font-light leading-tight text-center ease-out duration-200 group-hover:text-white-200 group-focus:text-white-200 lg:text-20 lg:hidden lg:group-hover:block lg:group-focus:block">
           {title}
         </p>
         <StarRating rating={rating} />
--- src/components/poster/Poster.tsx
+++ src/components/poster/Poster.tsx
@@ -7,7 +7,7 @@ const Poster = ({ imageUrl, alt }: PosterTypes) => {
   return (
     <img
       src={imageUrl}
-      className="block h-full w-full rounded-8 object-center object-cover shadow-lg shadow-black/50 ease-out duration-200 group-hover:shadow-none lg:group-hover:opacity-10 lg:group-focus:opacity-10"
+      className="block w-full rounded-8 object-center object-cover shadow-lg shadow-black/50 ease-out duration-200 group-hover:shadow-none lg:group-hover:opacity-10 lg:group-focus:opacity-10 aspect-2/3"
       alt={alt}
     />
   );
