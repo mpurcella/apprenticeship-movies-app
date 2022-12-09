bc6289ba95a5a8dc84f42d0257c3d9d0ee943f94
Michael Purcella <mjpurcella@gmail.com>
2022-12-09 11:53:00 -0800

    [#6] Refactored heading styling - PR #68

--- src/components/movie-layout/MovieLayout.tsx
+++ src/components/movie-layout/MovieLayout.tsx
@@ -8,8 +8,8 @@ type MovieLayoutTypes = {
 const MovieLayout = ({ label, children }: MovieLayoutTypes) => {
   return (
     <div className="flex flex-col items-center px-48 pt-102 pb-68 lg:py-56 lg:ml-260">
-      <h1 className="inline-block text-20 text-white-200 bg-black uppercase font-semibold text-center mb-36 px-8 py-4">
-        {label}
+      <h1 className="inline-block text-24 uppercase font-light text-center mb-44">
+        {label} Movies
       </h1>
       {children}
     </div>
