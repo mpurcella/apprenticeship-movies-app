bcb23d9d79e6c3e4e8c6b4cd8b0c730ede79bb0d
Michael Purcella <mjpurcella@gmail.com>
2022-12-09 12:03:02 -0800

    Refactor header storybook to use store context

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Header from "../components/header/Header";
+import { StoreCtx } from "../store/store";
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 export default {
@@ -7,6 +8,30 @@ export default {
   component: Header,
 } as ComponentMeta<typeof Header>;
 
-const Template: ComponentStory<typeof Header> = (args) => <Header />;
+const Template: ComponentStory<typeof Header> = () => (
+  <StoreCtx.Provider
+    value={{
+      genres: [
+        {
+          id: 1,
+          name: "Action",
+        },
+        {
+          id: 2,
+          name: "Comedy",
+        },
+        {
+          id: 3,
+          name: "Drama",
+        },
+      ],
+      config: {
+        baseUrl: "",
+      },
+    }}
+  >
+    <Header />
+  </StoreCtx.Provider>
+);
 
 export const Primary = Template.bind({});
