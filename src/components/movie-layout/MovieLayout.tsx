07c04aa82af40e260a92175f9cac301240c7d9f1
Michael Purcella <mjpurcella@gmail.com>
2022-12-14 01:00:59 -0800

    [#6] Refactor movie layout heading when label is null

--- src/components/movie-layout/MovieLayout.tsx
+++ src/components/movie-layout/MovieLayout.tsx
@@ -7,10 +7,12 @@ type MovieLayoutTypes = {
 
 const MovieLayout = ({ label, children }: MovieLayoutTypes) => {
   return (
-    <div className="flex flex-col items-center px-48 pt-102 pb-68 lg:py-56 lg:ml-260">
-      <h1 className="inline-block text-24 uppercase font-light text-center mb-44">
-        {label} Movies
-      </h1>
+    <div className="flex flex-col items-center px-48 pt-102 pb-68 lg:py-68 lg:ml-260">
+      {label === null ? null : (
+        <h1 className="inline-block text-24 uppercase font-light  mb-44">
+          {label} Movies
+        </h1>
+      )}
       {children}
     </div>
   );
