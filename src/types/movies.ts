2e3229a4e3ed8a25239045ff462cffcd058953d3
Michael Purcella <mjpurcella@gmail.com>
2022-12-14 00:43:25 -0800

    Add movie detail type and refactor movie type

--- src/types/movies.ts
+++ src/types/movies.ts
@@ -8,6 +8,48 @@ export type Genre = {
 export type Movie = {
   id: number;
   title: string;
-  poster_path: string;
+  poster_path: string | null;
+  vote_average: number;
+};
+
+export type MovieDetails = {
+  credits: {
+    cast: Array<{
+      name: string;
+      id: number;
+      profile_path: string | null;
+    }>;
+  };
+  genres: Array<{
+    id: number;
+    name: string;
+  }>;
+  homepage: string | null;
+  imdb_id: string | null;
+  overview: string | null;
+  poster_path: string | null;
+  recommendations: {
+    results: Array<{
+      id: number;
+      poster_path: string | null;
+      title: string;
+      vote_average: number;
+    }>;
+  };
+  release_date: string;
+  runtime: number | null;
+  spoken_languages: Array<{
+    name: string;
+  }>;
+  tagline: string | null;
+  title: string;
+  videos: {
+    results: Array<{
+      id: number;
+      key: string;
+      name: string;
+      site: string;
+    }>;
+  };
   vote_average: number;
 };
