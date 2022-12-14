e2fb9421e2b5d0d8f25208b95f79cb8a5ad6de3f
Michael Purcella <mjpurcella@gmail.com>
2022-12-14 00:39:34 -0800

    [#62] Create cast list component

---
+++ src/components/cast-list/CastList.tsx
@@ -0,0 +1,37 @@
+import missingPoster from "../../images/missingposter.jpg";
+
+type CastListTypes = {
+  cast: Array<{
+    name: string;
+    id: number;
+    profile_path: string | null;
+  }>;
+  baseUrl: string;
+};
+
+const CastList = ({ cast, baseUrl }: CastListTypes) => {
+  return (
+    <ul className="flex flex-wrap space-x-8">
+      {cast.slice(0, 6).map((item) => {
+        return (
+          <li
+            className="max-w-[75px] max-h-[75px] group relative"
+            key={item.id}
+          >
+            <img
+              src={
+                item.profile_path === null
+                  ? missingPoster
+                  : `${baseUrl}w780/${item.profile_path}`
+              }
+              alt={item.name}
+              className="w-full h-full object-cover object-center rounded-50 border border-grey-200"
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default CastList;
---
+++ src/stories/CastList.stories.tsx
@@ -0,0 +1,48 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import CastList from "../components/cast-list/CastList";
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+export default {
+  title: "CastList",
+  component: CastList,
+} as ComponentMeta<typeof CastList>;
+
+const Template: ComponentStory<typeof CastList> = (args) => (
+  <CastList {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  cast: [
+    {
+      name: "Test",
+      id: 1,
+      profile_path: null,
+    },
+    {
+      name: "Test",
+      id: 2,
+      profile_path: null,
+    },
+    {
+      name: "Test",
+      id: 3,
+      profile_path: null,
+    },
+    {
+      name: "Test",
+      id: 4,
+      profile_path: null,
+    },
+    {
+      name: "Test",
+      id: 5,
+      profile_path: null,
+    },
+    {
+      name: "Test",
+      id: 6,
+      profile_path: null,
+    },
+  ],
+};
