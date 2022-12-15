b3d9cda478abf2a3f08233063158ba08937ab6e7
Michael Purcella <mjpurcella@gmail.com>
2022-12-14 22:15:43 -0800

    [#53] Refactor MovieInfo component and update MovieInfo story

--- src/components/movie-info/MovieInfo.tsx
+++ src/components/movie-info/MovieInfo.tsx
@@ -5,14 +5,17 @@ import { FaExternalLinkAlt, FaPlay } from "react-icons/fa";
 import { BiMovie } from "react-icons/bi";
 import ButtonLinkTertiary from "../button-link-tertiary/ButtonLinkTertiary";
 import CastList from "../cast-list/CastList";
-import missingPoster from "../../images/missingposter.jpg";
+import missingPoster from "../../images/missing-poster.jpg";
 import { Link } from "react-router-dom";
 import MovieList from "../movie-list/MovieList";
+import { SpokenLanguage } from "../../types/movies";
+import classNames from "classnames";
 
 type MovieInfoTypes = {
   baseUrl: string;
   credits: {
     cast: Array<{
+      character: string;
       name: string;
       id: number;
       profile_path: string | null;
@@ -47,10 +50,18 @@ type MovieInfoTypes = {
     results: Array<{
       id: number;
       key: string;
-      name: string;
       site: string;
+      type: string;
     }>;
   };
+};
+
+const getSpokenLanguage = (spokenLanguages: SpokenLanguage[]) => {
+  if (spokenLanguages.length === 0) {
+    return null;
+  }
+
+  return spokenLanguages[0].english_name ?? spokenLanguages[0].name ?? null;
 };
 
 const MovieInfo = ({
@@ -70,20 +81,34 @@ const MovieInfo = ({
   videos,
   voteAverage,
 }: MovieInfoTypes) => {
+  const language = getSpokenLanguage(spokenLanguages);
+
   return (
-    <>
-      <div className="flex items-center space-x-36">
-        <div className="w-[40%]">
+    <div className="divide-y-2 divide-grey-100">
+      <div
+        className={classNames(
+          "flex flex-col lg:space-x-44 pt-40 pb-68 xl:flex-row",
+          {
+            "pb-0":
+              recommendations.results.length === 0 && credits.cast.length === 0,
+          }
+        )}
+      >
+        <div className="flex justify-center mb-68 xl:mb-0 xl:w-35">
           {posterPath === null ? (
             <Poster imageUrl={missingPoster} alt={title} />
           ) : (
-            <Poster imageUrl={`${baseUrl}w780/${posterPath}`} alt={title} />
+            <Poster imageUrl={`${baseUrl}w780${posterPath}`} alt={title} />
           )}
         </div>
-        <div className="w-[60%]">
-          <div className=" mb-28">
-            <h1 className="text-[40px] font-bold uppercase mb-16">{title}</h1>
-            {Boolean(tagline) && <h2 className="uppercase">{tagline}</h2>}
+        <div className="xl:w-65">
+          <div className="mb-28">
+            <h1 className="text-32 lg:text-40 font-bold uppercase mb-16">
+              {title}
+            </h1>
+            {Boolean(tagline) && (
+              <h2 className="uppercase leading-tight">{tagline}</h2>
+            )}
           </div>
           <div className="flex justify-between mb-56">
             {voteAverage === 0 ? (
@@ -95,75 +120,54 @@ const MovieInfo = ({
               />
             )}
             <div className="flex space-x-8">
-              {spokenLanguages.map((language, i) => {
-                if (i === 0 && Boolean(language.english_name)) {
-                  return (
-                    <li
-                      className="list-none text-14 text-grey-500"
-                      key={language.english_name}
-                    >
-                      {language.english_name}
-                      <span className="ml-8">/</span>
-                    </li>
-                  );
-                } else if (i === 0 && Boolean(language.name)) {
-                  return (
-                    <li
-                      className="list-none text-14 text-grey-500"
-                      key={language.name}
-                    >
-                      {language.name}
-                      <span className="ml-8">/</span>
-                    </li>
-                  );
-                } else {
-                  return null;
-                }
-              })}
-              {Boolean(runtime) && (
-                <span className="uppercase text-14 text-grey-500">
-                  {runtime} Min.<span className="ml-8">/</span>
+              {Boolean(language) && (
+                <span
+                  className="list-none text-14 text-grey-500"
+                  key={language}
+                >
+                  {language}
                 </span>
               )}
-              <span className="text-14 text-grey-500">
-                {releaseDate.slice(0, 4)}
-              </span>
-            </div>
-          </div>
-          <div className="mb-36">
-            <h3 className="font-bold uppercase mb-12">Genres:</h3>
-            <ul className="flex space-x-16">
-              {genres.map((genre) => {
-                return (
-                  <Link
-                    to={`/genres/${genre.name}`}
-                    className="button-link-tertiary"
-                    key={genre.id}
-                  >
-                    {genre.name}
-                  </Link>
-                );
-              })}
-            </ul>
-          </div>
-          <div className="mb-36">
-            <h3 className="font-bold uppercase mb-12">Synopsis:</h3>
-            {Boolean(overview) && <p className="leading-snug">{overview}</p>}
-          </div>
-          <div className="mb-56">
-            <h3 className="font-bold uppercase mb-12">Top Cast:</h3>
-            <div className="flex space-x-12 items-end">
-              <CastList cast={credits.cast} baseUrl={baseUrl} />
-              {imdbId === null ? null : (
-                <ButtonLinkTertiary
-                  url={`https://www.imdb.com/title/${imdbId}/fullcredits/?ref_=tt_cl_sm`}
-                >
-                  View All Cast
-                </ButtonLinkTertiary>
+              {Boolean(runtime) && (
+                <span className="uppercase text-14 text-grey-500">
+                  <span className="mr-8">/</span>
+                  {runtime} Min.
+                </span>
+              )}
+              {Boolean(releaseDate) && (
+                <span className="text-14 text-grey-500">
+                  <span className="mr-8">/</span>
+                  {releaseDate.slice(0, 4)}
+                </span>
               )}
             </div>
           </div>
-          <div className="space-x-20">
+          {genres.length === 0 ? null : (
+            <div className="mb-36">
+              <h3 className="font-bold uppercase mb-12">Genres:</h3>
+              <ul className="flex space-x-16">
+                {genres.map((genre) => {
+                  return (
+                    <Link
+                      to={`/genres/${genre.name}`}
+                      className="button-link-tertiary"
+                      key={genre.id}
+                    >
+                      {genre.name}
+                    </Link>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+          {Boolean(overview) && (
+            <div className="mb-36">
+              <h3 className="font-bold uppercase mb-12">Synopsis:</h3>
+              <p className="leading-normal">{overview}</p>
+            </div>
+          )}
+
+          <div className="flex flex-wrap justify-center gap-20 lg:space-x-20">
             {homepage === null || homepage === "" ? null : (
               <ButtonLinkPrimary
                 url={homepage}
@@ -182,37 +186,59 @@ const MovieInfo = ({
                 IMDB
               </ButtonLinkPrimary>
             )}
-            {videos.results.map((video) => {
-              if (
-                video.name.includes("Official Trailer") &&
-                video.site === "YouTube"
-              ) {
-                return (
-                  <ButtonLinkPrimary
-                    url={`https://www.youtube.com/watch?v=${video.key}`}
-                    key={video.id}
-                    styleLight={false}
-                    endIcon={<FaPlay />}
-                  >
-                    Trailer
-                  </ButtonLinkPrimary>
-                );
-              } else {
-                return null;
-              }
-            })}
+            {videos.results
+              .filter(
+                (video) => video.type === "Trailer" && video.site === "YouTube"
+              )
+              .map((filteredVideo, i) => {
+                if (i === 0) {
+                  return (
+                    <ButtonLinkPrimary
+                      url={`https://www.youtube.com/watch?v=${filteredVideo.key}`}
+                      key={filteredVideo.id}
+                      styleLight={false}
+                      endIcon={<FaPlay />}
+                    >
+                      Trailer
+                    </ButtonLinkPrimary>
+                  );
+                } else {
+                  return null;
+                }
+              })}
           </div>
         </div>
       </div>
+      {credits.cast.length === 0 ? null : (
+        <div
+          className={classNames("py-68", {
+            "pt-68 pb-40": recommendations.results.length === 0,
+          })}
+        >
+          <h3 className="text-24 lg:text-32 font-bold uppercase mb-44">
+            Top Cast:
+          </h3>
+          <div className="flex flex-col items-center">
+            <CastList cast={credits.cast} baseUrl={baseUrl} imdbId={imdbId} />
+            {imdbId === null ? null : (
+              <ButtonLinkTertiary
+                url={`https://www.imdb.com/title/${imdbId}/fullcredits/?ref_=tt_cl_sm`}
+              >
+                View All Cast
+              </ButtonLinkTertiary>
+            )}
+          </div>
+        </div>
+      )}
       {recommendations.results.length === 0 ? null : (
-        <div className="flex flex-col items-center mt-68">
-          <h2 className="inline-block text-24 uppercase font-light mb-44">
+        <div className="pt-68 lg:pb-40">
+          <h2 className="text-24 lg:text-32 font-bold uppercase mb-44">
             Recommended Movies
           </h2>
           <MovieList movies={recommendations.results} baseUrl={baseUrl} />
         </div>
       )}
-    </>
+    </div>
   );
 };
 
