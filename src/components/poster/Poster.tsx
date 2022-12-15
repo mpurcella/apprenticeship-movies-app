6a4d6378eabe74368a1fb88bf90a51e58cffd0b6
Michael Purcella <mjpurcella@gmail.com>
2022-12-14 22:51:28 -0800

    [#56] Add max-width to poster and change missing-poster url

--- src/components/poster/Poster.tsx
+++ src/components/poster/Poster.tsx
@@ -1,4 +1,4 @@
-import missingPoster from "../../images/missingposter.jpg";
+import missingPoster from "../../images/missing-poster.jpg";
 
 type PosterTypes = {
   imageUrl: string | null;
@@ -9,7 +9,7 @@ const Poster = ({ imageUrl, alt }: PosterTypes) => {
   return (
     <img
       src={imageUrl === null ? missingPoster : imageUrl}
-      className="block w-full rounded-8 object-center object-cover shadow-lg shadow-black/50 ease-out duration-200 group-hover:shadow-none lg:group-hover:opacity-10 lg:group-focus:opacity-10 aspect-2/3"
+      className="block w-full sm:max-w-400 rounded-8 object-center object-cover shadow-lg shadow-black/50 ease-out duration-200 group-hover:shadow-none lg:group-hover:opacity-10 lg:group-focus:opacity-10 aspect-2/3"
       loading="lazy"
       alt={alt}
     />
