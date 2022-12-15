769ea1e22a2c50298c234ce391d8939b40c6c51c
Michael Purcella <mjpurcella@gmail.com>
2022-12-14 22:47:36 -0800

    [#6] Refactor movie layout component styling

--- src/components/movie-layout/MovieLayout.tsx
+++ src/components/movie-layout/MovieLayout.tsx
@@ -7,9 +7,9 @@ type MovieLayoutTypes = {
 
 const MovieLayout = ({ label, children }: MovieLayoutTypes) => {
   return (
-    <div className="flex flex-col items-center px-48 pt-102 pb-68 lg:py-68 lg:ml-260">
+    <div className="px-20 pt-132 pb-68 md:px-68 lg:p-68 lg:ml-260">
       {label === null ? null : (
-        <h1 className="inline-block text-24 uppercase font-light  mb-44">
+        <h1 className="text-24 font-light uppercase mb-44 lg:text-32">
           {label} Movies
         </h1>
       )}
