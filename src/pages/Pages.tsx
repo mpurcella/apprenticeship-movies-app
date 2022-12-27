206bdaa9bd6865e662e747c319aa56c117c4a249
Michael Purcella <mjpurcella@gmail.com>
2022-12-27 13:39:32 -0800

    Refactor router to use form and add route for homepage

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,6 +1,13 @@
 import { lazy, Suspense } from "react";
-import { Route, Routes, Navigate, Outlet } from "react-router-dom";
+import {
+  Route,
+  Navigate,
+  Outlet,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Header from "../components/header/Header";
+import Home from "./home/Home";
 
 const Popular = lazy(async () => await import("./discover/Popular"));
 const TopRated = lazy(async () => await import("./discover/TopRated"));
@@ -9,71 +16,64 @@ const Genres = lazy(async () => await import("./genres/Genres"));
 const Movie = lazy(async () => await import("./movie/Movie"));
 const Actor = lazy(async () => await import("./actor/Actor"));
 
-function Pages() {
-  return (
-    <>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route
-            path="/"
-            element={<Navigate replace to="/discover/popular" />}
-          />
-          <Route
-            path="/discover/popular"
-            element={
-              <Suspense fallback={<div>Loading popular movies...</div>}>
-                <Popular />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/discover/top-rated"
-            element={
-              <Suspense fallback={<div>Loading top rated movies...</div>}>
-                <TopRated />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/discover/upcoming"
-            element={
-              <Suspense fallback={<div>Loading upcoming movies...</div>}>
-                <Upcoming />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/genres/:genre"
-            element={
-              <Suspense fallback={<div>Loading genres...</div>}>
-                <Genres />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/movie/:id"
-            element={
-              <Suspense fallback={<div>Loading movie...</div>}>
-                <Movie />
-              </Suspense>
-            }
-          />
-          <Route path="/actor" element={<Actor />} />
-          <Route path="/not-found" element={<div>Page not found.</div>} />
-          <Route path="*" element={<Navigate replace to="/not-found" />} />
-        </Route>
-      </Routes>
-    </>
-  );
-}
+const pages = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<MainLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/discover/popular"
+        element={
+          <Suspense fallback={<div>Loading popular movies...</div>}>
+            <Popular />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/discover/top-rated"
+        element={
+          <Suspense fallback={<div>Loading top rated movies...</div>}>
+            <TopRated />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/discover/upcoming"
+        element={
+          <Suspense fallback={<div>Loading upcoming movies...</div>}>
+            <Upcoming />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/genres/:genre"
+        element={
+          <Suspense fallback={<div>Loading genres...</div>}>
+            <Genres />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/movie/:id"
+        element={
+          <Suspense fallback={<div>Loading movie...</div>}>
+            <Movie />
+          </Suspense>
+        }
+      />
+      <Route path="/actor" element={<Actor />} />
+      <Route path="/not-found" element={<div>Page not found</div>} />
+      <Route path="*" element={<Navigate replace to="/not-found" />} />
+    </Route>
+  )
+);
+
+export default pages;
 
 function MainLayout() {
   return (
-    <div>
+    <>
       <Header />
       <Outlet />
-    </div>
+    </>
   );
 }
-
-export default Pages;
