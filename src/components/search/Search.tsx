f6c9b211e30f3b0b5d8e1c72ecf0465037a12b73
Michael Purcella <mjpurcella@gmail.com>
2022-12-27 15:01:35 -0800

    Refactor search input focus

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -6,7 +6,7 @@ const Search = ({ placeholder }: SearchTypes) => {
   return (
     <input
       type="search"
-      className="w-full bg-white-200 px-16 py-12 rounded-8 outline-none duration-200 ease-out focus:ring-red/75 focus:ring-4"
+      className="w-full bg-white-200 px-16 py-12 rounded-8 outline-none duration-200 ease-out focus-visible:ring-red focus-visible:ring-4"
       placeholder={placeholder}
       disabled
     />
