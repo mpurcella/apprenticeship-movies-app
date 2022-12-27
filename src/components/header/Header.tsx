799892271d052d3ce780d3d688e1f65c43548a83
Michael Purcella <mjpurcella@gmail.com>
2022-12-27 15:24:45 -0800

    [#63] Refactor header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import HamburgerButton from "../hamburger-button/HamburgerButton";
-import Sidebar from "../sidebar/Sidebar";
+import Navbar from "../navbar/Navbar";
+import popcornLogo from "../../images/popcorn.png";
 
 function useMediaQuery(mq: string) {
   const [mql] = useState(() => window.matchMedia(mq));
@@ -22,27 +24,61 @@ function useMediaQuery(mq: string) {
 }
 
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(() => false);
+  const [isGenreListOpen, setIsGenreListOpen] = useState(() => false);
   const isDesktop = useMediaQuery("(min-width: 1023px)");
-  const [isOpen, setIsOpen] = useState(() => false);
 
-  const handleIsOpen = () => {
-    setIsOpen((currentIsDesktop) => !currentIsDesktop);
+  const handleIsNavOpen = () => {
+    setIsNavOpen((currentIsNavOpen) => !currentIsNavOpen);
   };
 
-  const closeSidebar = () => {
-    setIsOpen(false);
+  const handleIsGenreListOpen = () => {
+    setIsGenreListOpen((currentIsGenreListOpen) => !currentIsGenreListOpen);
+  };
+
+  const closeNav = () => {
+    setIsNavOpen(false);
+  };
+
+  const closeGenreList = () => {
+    setIsGenreListOpen(false);
   };
 
   useEffect(() => {
     if (!isDesktop) {
-      setIsOpen(false);
+      setIsNavOpen(false);
+      setIsGenreListOpen(false);
     }
   }, [isDesktop]);
 
   return (
-    <header className="w-full fixed top-0 bg-white shadow-xl shadow-black/15 lg:bg-transparent p-20 z-50 lg:p-0">
-      <HamburgerButton onClick={handleIsOpen} />
-      <Sidebar isOpen={isDesktop || isOpen} closeSidebar={closeSidebar} />
+    <header className="h-68 bg-black flex justify-between items-center shadow-xl shadow-black/30 px-20 z-20 sticky top-0 left-0 md:px-68 lg:gap-44">
+      <Link
+        to="/"
+        className="inline-flex items-center z-30 py-2 px-4  outline-none focus-visible:outline-1 focus-visible:outline-red"
+        aria-label="Homepage"
+        onClick={() => {
+          closeGenreList();
+          closeNav();
+        }}
+      >
+        <img src={popcornLogo} alt="Popcorn Logo" className="w-[40px]" />
+      </Link>
+      <HamburgerButton
+        isNavOpen={isNavOpen}
+        onClick={() => {
+          closeGenreList();
+          handleIsNavOpen();
+        }}
+      />
+      <Navbar
+        isNavOpen={isNavOpen}
+        isGenreListOpen={isGenreListOpen}
+        handleIsNavOpen={handleIsNavOpen}
+        handleIsGenreListOpen={handleIsGenreListOpen}
+        closeNav={closeNav}
+        closeGenreList={closeGenreList}
+      />
     </header>
   );
 };
