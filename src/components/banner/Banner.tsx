7a3202cc73c2cb1cc25ab7068c9fef24588aabf0
Michael Purcella <mjpurcella@gmail.com>
2022-12-27 15:28:18 -0800

    Refactor banner component

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -4,12 +4,10 @@ type BannerTypes = {
 
 const Banner = ({ label }: BannerTypes) => {
   return (
-    <div className="bg-red relative h-[15vh]">
-      <div className="w-full h-full flex justify-center items-center absolute top-0 left-0 ">
-        <h1 className="text-28 text-white-200 uppercase font-extrabold md:text-32">
-          {label} Movies
-        </h1>
-      </div>
+    <div className="bg-red flex justify-center items-center h-15">
+      <h1 className="text-28 text-white-200 uppercase font-extrabold md:text-32">
+        {label} Movies
+      </h1>
     </div>
   );
 };
