f13774506fd629be0846ceff636ab7f9c2ca44ec
Michael Purcella <mjpurcella@gmail.com>
2022-12-27 15:16:18 -0800

    [#6] Refactor movie layout component to include banner component

--- src/components/movie-layout/MovieLayout.tsx
+++ src/components/movie-layout/MovieLayout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Banner from "../banner/Banner";
 
 type MovieLayoutTypes = {
   label: React.ReactNode;
@@ -7,14 +8,10 @@ type MovieLayoutTypes = {
 
 const MovieLayout = ({ label, children }: MovieLayoutTypes) => {
   return (
-    <div className="px-20 pt-132 pb-68 md:px-68 lg:p-68 lg:ml-260">
-      {label === null ? null : (
-        <h1 className="text-24 font-light uppercase mb-44 lg:text-32">
-          {label} Movies
-        </h1>
-      )}
-      {children}
-    </div>
+    <main>
+      {label === null ? null : <Banner label={label} />}
+      <div className="m-auto px-20 py-68 md:px-68">{children}</div>
+    </main>
   );
 };
 
