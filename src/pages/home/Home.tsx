780228f0d0594cd21bcb943394fcd2d8b505ef27
Michael Purcella <mjpurcella@gmail.com>
2022-12-27 13:37:33 -0800

    Create homepage for site

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import Hero from "../../components/hero/Hero";
+
+const Home = () => {
+  return (
+    <main className="h-[calc(100vh-68px)]">
+      <section className="h-full">
+        <Hero />
+      </section>
+    </main>
+  );
+};
+
+export default Home;
