9282fae83f59c45eb39d1ebabeee3e74858142d8
Michael Purcella <mjpurcella@gmail.com>
2022-12-29 13:22:42 -0800

    Remove rounded corners and refactor padding

--- src/components/movie-card/MovieCard.tsx
+++ src/components/movie-card/MovieCard.tsx
@@ -13,10 +13,10 @@ const MovieCard = ({ imageUrl, url, title, alt, rating }: MovieCardTypes) => {
   return (
     <a
       href={url}
-      className="rounded-8 group ease-out duration-200 flex flex-col relative outline-none hover:bg-black hover:scale-102 hover:shadow-lg hover:shadow-black/50 focus:bg-black focus:scale-102 lg:bg-black"
+      className="group ease-out duration-200 flex flex-col relative outline-none hover:bg-black hover:scale-102 hover:shadow-lg hover:shadow-black/50 focus:bg-black focus:scale-102 lg:bg-black"
     >
       <Poster imageUrl={imageUrl} alt={alt} />
-      <div className="flex flex-col items-center px-12 py-20 space-y-12 lg:space-y-20 lg:w-full lg:h-full lg:absolute lg:justify-center lg:top-0 lg:left-0">
+      <div className="flex flex-col items-center p-20 space-y-12 lg:space-y-20 lg:w-full lg:h-full lg:absolute lg:justify-center lg:top-0 lg:left-0">
         <p className="text-16 font-light leading-tight text-center ease-out duration-200 group-hover:text-white-200 group-focus:text-white-200 lg:text-20 lg:hidden lg:group-hover:block lg:group-focus:block">
           {title}
         </p>
