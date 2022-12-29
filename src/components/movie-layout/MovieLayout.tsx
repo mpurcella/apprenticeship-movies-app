293dc273aeb35b68bfa45e3ba19e831b4a69ee11
Michael Purcella <mjpurcella@gmail.com>
2022-12-29 13:24:43 -0800

    Remove label from types and refactor styling

--- src/components/movie-layout/MovieLayout.tsx
+++ src/components/movie-layout/MovieLayout.tsx
@@ -1,18 +1,11 @@
 import React from "react";
-import Banner from "../banner/Banner";
 
 type MovieLayoutTypes = {
-  label: React.ReactNode;
   children: React.ReactNode;
 };
 
-const MovieLayout = ({ label, children }: MovieLayoutTypes) => {
-  return (
-    <main>
-      {label === null ? null : <Banner label={label} />}
-      <div className="m-auto px-20 py-68 md:px-68">{children}</div>
-    </main>
-  );
+const MovieLayout = ({ children }: MovieLayoutTypes) => {
+  return <main className="px-20 py-68 md:px-68">{children}</main>;
 };
 
 export default MovieLayout;
