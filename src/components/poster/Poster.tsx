dfe5989f3fe21245a1fc6cfca34280fac9492b6b
Michael Purcella <mjpurcella@gmail.com>
2022-12-29 13:20:59 -0800

    Remove rounded corners

--- src/components/poster/Poster.tsx
+++ src/components/poster/Poster.tsx
@@ -9,7 +9,7 @@ const Poster = ({ imageUrl, alt }: PosterTypes) => {
   return (
     <img
       src={imageUrl === null ? missingPoster : imageUrl}
-      className="block w-full sm:max-w-400 rounded-8 object-center object-cover shadow-lg shadow-black/50 ease-out duration-200 group-hover:shadow-none lg:group-hover:opacity-10 lg:group-focus:opacity-10 aspect-2/3"
+      className="block w-full sm:max-w-400 object-center object-cover shadow-lg shadow-black/50 ease-out duration-200 group-hover:shadow-none lg:group-hover:opacity-10 lg:group-focus:opacity-10 aspect-2/3"
       loading="lazy"
       alt={alt}
     />
