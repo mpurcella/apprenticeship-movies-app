40e9d525592fc791fd3a9789b01d499d339d88d5
Michael Purcella <mjpurcella@gmail.com>
2023-01-10 12:47:29 -0800

    Refactor button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 
 type ButtonTypes = {
-  startIcon: React.ReactNode;
-  endIcon: React.ReactNode;
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
   children: React.ReactNode;
   styleLight: boolean;
+  onClick: () => void;
 };
 
-const Button = ({ startIcon, endIcon, children, styleLight }: ButtonTypes) => {
+const Button = ({
+  startIcon,
+  endIcon,
+  children,
+  styleLight,
+  onClick,
+}: ButtonTypes) => {
   return (
-    <button className={styleLight ? "button-light" : "button-dark"}>
+    <button
+      className={styleLight ? "button-light" : "button-dark"}
+      onClick={onClick}
+    >
       {Boolean(startIcon) && <span className="text-14 mr-12">{startIcon}</span>}
       {children}
       {Boolean(endIcon) && <span className="text-14 ml-12">{endIcon}</span>}
