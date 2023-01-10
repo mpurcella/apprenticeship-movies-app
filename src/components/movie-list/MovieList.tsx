df29ee052965422cc303f070fb0ef52afe4ed9ac
Michael Purcella <mjpurcella@gmail.com>
2023-01-10 12:57:47 -0800

    Refactor movie list component

--- src/components/movie-list/MovieList.tsx
+++ src/components/movie-list/MovieList.tsx
@@ -1,29 +1,29 @@
-import { Movie } from "../../types/movies";
+import { Movies } from "../../types/movies";
 import MovieCard from "../movie-card/MovieCard";
 
 type MovieListTypes = {
-  movies: Movie[];
-  baseUrl: string;
+  movies: Movies;
+  baseImageUrl: string;
   label?: string;
 };
 
-const MovieList = ({ movies, baseUrl, label }: MovieListTypes) => {
+const MovieList = ({ movies, baseImageUrl, label }: MovieListTypes) => {
   return (
     <section>
       {label === undefined ? null : (
-        <h1 className="text-24 text-black uppercase font-extrabold flex justify-center mb-40 md:text-28">
+        <h1 className="text-24 text-black uppercase font-extrabold flex justify-center mb-40 md:text-24">
           {label} Movies
         </h1>
       )}
-      <ul className="grid grid-cols-1 gap-36 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {movies.map((movie) => {
+      <ul className="grid grid-cols-1 gap-36 mb-0 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {movies.results.map((movie) => {
           return (
             <li key={movie.id}>
               <MovieCard
                 imageUrl={
                   movie.poster_path === null
                     ? null
-                    : `${baseUrl}w780${movie.poster_path}`
+                    : `${baseImageUrl}w780${movie.poster_path}`
                 }
                 url={`/movie/${movie.id}`}
                 title={movie.title}
